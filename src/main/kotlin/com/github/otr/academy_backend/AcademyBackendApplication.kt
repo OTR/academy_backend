7481a5e126ca1069c198da9e8495c9cc0e9f4f15
OTR <49165533+OTR@users.noreply.github.com>
2023-07-08 09:56:07 +0600

    [CHORE] Initial commit

---
+++ src/main/kotlin/com/github/otr/academy_backend/AcademyBackendApplication.kt
@@ -0,0 +1,14 @@
+package com.github.otr.academy_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+/**
+ *
+ */
+@SpringBootApplication
+class AcademyBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<AcademyBackendApplication>(*args)
+}
