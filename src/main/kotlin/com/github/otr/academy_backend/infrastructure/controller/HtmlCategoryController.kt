bdeb728fed8096e13598c6d3e779d8589738cf90
OTR <49165533+OTR@users.noreply.github.com>
2023-07-10 13:00:02 +0600

    [FEAT] Implement parsing HTTP GET parameters

--- src/main/kotlin/com/github/otr/academy_backend/infrastructure/controller/HtmlCategoryController.kt
+++ src/main/kotlin/com/github/otr/academy_backend/infrastructure/controller/HtmlCategoryController.kt
@@ -3,11 +3,15 @@ package com.github.otr.academy_backend.infrastructure.controller
 import com.github.otr.academy_backend.domain.model.Category
 import com.github.otr.academy_backend.use_case.GetAllCategoriesUseCase
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 /**
  *
@@ -18,17 +22,20 @@ class HtmlCategoryController(
     private val getAllCategoriesUseCase: GetAllCategoriesUseCase
 ) {
 
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
     companion object {
         private const val DEFAULT_TITLE: String = "Default Title"
         private const val TRACKS_TITLE_KEY: String = "title.tracks"
     }
 
-    @GetMapping(path=["/tracks"])
-    fun getTracksPage(model: Model): String {
-        model["title"] = environment.getProperty(
-            TRACKS_TITLE_KEY,
-            DEFAULT_TITLE
-        )
+    @GetMapping(path=["/tracks?category={category_id}"])
+    fun getTracksPage(
+        model: Model,
+        @PathVariable("category_id") categoryId: Int
+    ): String {
+        logger.info("Category ID is: $categoryId")
+        model["title"] = environment.getProperty(TRACKS_TITLE_KEY, DEFAULT_TITLE)
         val categories: List<Category> = getAllCategoriesUseCase()
         model["categories"] = categories
         return "tracks"
--- src/main/kotlin/com/github/otr/academy_backend/infrastructure/controller/RestController.kt
+++ src/main/kotlin/com/github/otr/academy_backend/infrastructure/controller/RestController.kt
@@ -1,80 +1,96 @@
 package com.github.otr.academy_backend.infrastructure.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.otr.academy_backend.domain.model.Category
+import com.github.otr.academy_backend.domain.repository.CategoryRepository
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
+
 import org.springframework.web.bind.annotation.RestController
+import java.io.Serializable
 
 /**
- * GET / return greeting message
- * GET /api/text/all return all texts
- * GET /api/text/{id} return a text by the given ID
- * POST /api/text/new create a new text with fields supplied as JSON in request body
- * PUT /api/text/{id} update a text by the given ID with fields supplied as JSON
+ *  * GET /api/track-categories return all categories
+ *  * GET /api/track-categories/{id} return a category by the given ID
+ *  * POST /api/track-categories/new create a new category with fields supplied as JSON in request body
+ *  * PUT /api/track-categories/{id} update a category by the given ID with fields supplied as JSON
  *                in request body
- * DELETE /api/text/{id} deleteById a text by the given ID
+ *  * DELETE /api/track-categories/{id} delete a category by the given ID
  */
 @RestController
-class RestController {
+class RestCategoryController(
+    private val repository: CategoryRepository,
+    private val objectMapper: ObjectMapper = jacksonObjectMapper()
+) {
+
+    companion object {
+        private const val BASE_API_URL: String = "/api/track-categories"
+    }
+
+    @PostMapping(path = ["$BASE_API_URL/new"])
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createNewCategory(@RequestBody newEntity: Category): Category {
+        return repository.create(newEntity)
+    }
+
+    @GetMapping(path = [BASE_API_URL])
+    fun getAllCategories(): List<Category> {
+        return repository.getAll()
+    }
+
+    @GetMapping(path = ["$BASE_API_URL/{id}"])
+    fun getCategoryById(@PathVariable("id") id: Int): ResponseEntity<Serializable> {
+        val retrievedCategory = repository.getById(id)
+        return if(retrievedCategory == null) {
+            ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(toJson("Text Entity with ID: $id not found"))
+        } else {
+            ResponseEntity
+                .ok()
+                .body(toJson(retrievedCategory))
+        }
+    }
+
+    @PutMapping(path = ["$BASE_API_URL/{id}"])
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    fun updateTextEntityById(
+        @PathVariable("id") id: Int,
+        @RequestBody updatedCategory: Category
+    ): Category {
+        return repository.update(updatedCategory)
+    }
+
+    @DeleteMapping(path = ["$BASE_API_URL/{id}"])
+    fun deleteTextEntityById(@PathVariable("id") id: Int): ResponseEntity<String> {
+        val retrievedTextEntity = repository.deleteById(id)
+        return if (retrievedTextEntity == null) {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                toJson("Text Entity with ID: $id not found")
+            )
+        } else {
+            ResponseEntity.status(HttpStatus.NO_CONTENT).body(
+                toJson("Text Entity with ID: $id deleted")
+            )
+        }
+    }
+
+
+    /**
+     * Serialize Any object to valid JSON
+     */
+    private fun toJson(value: Any): String {
+        return objectMapper.writeValueAsString(value)
+    }
+
 }
-//    @Autowired
-//    private lateinit var textService: TextService
-//
-//    companion object {
-//        private const val baseApiUrl = "/api/text"
-//        private val objectMapper: ObjectMapper = jacksonObjectMapper()
-//
-//        /**
-//         * Serialize Any object to valid JSON
-//         */
-//        private fun toJson(value: Any): String {
-//            return objectMapper.writeValueAsString(value)
-//        }
-//    }
-//
-//    @PostMapping(path = ["$baseApiUrl/new"])
-//    @ResponseStatus(HttpStatus.CREATED)
-//    fun createNewTextEntity(@RequestBody newTextEntity: TextEntity): TextEntity {
-//        return textService.create(newTextEntity)
-//    }
-//
-//    @GetMapping(path = ["$baseApiUrl/all"])
-//    fun getAllTextEntities(): List<TextEntity> {
-//        return textService.getAll()
-//    }
-//
-//    @GetMapping(path = ["$baseApiUrl/{id}"])
-//    fun getTextEntityById(@PathVariable("id") id: Int): ResponseEntity<Serializable> {
-//        val retrievedTextEntity = textService.getById(id)
-//        return if(retrievedTextEntity == null) {
-//            ResponseEntity
-//                .status(HttpStatus.NOT_FOUND)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .body(toJson("Text Entity with ID: $id not found"))
-//        } else {
-//            ResponseEntity
-//                .ok()
-//                .body(retrievedTextEntity)
-//        }
-//    }
-//
-//    @PutMapping(path = ["$baseApiUrl/{id}"])
-//    @ResponseStatus(HttpStatus.ACCEPTED)
-//    fun updateTextEntityById(
-//        @PathVariable("id") id: Int,
-//        @RequestBody updatedTextEntity: TextEntity
-//    ): TextEntity {
-//        return textService.updateById(id, updatedTextEntity)
-//    }
-//
-//    @DeleteMapping(path = ["$baseApiUrl/{id}"])
-//    fun deleteTextEntityById(@PathVariable("id") id: Int): ResponseEntity<String> {
-//        val retrievedTextEntity = textService.deleteById(id)
-//        return if (retrievedTextEntity == null) {
-//            ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-//                toJson("Text Entity with ID: $id not found")
-//            )
-//        } else {
-//            ResponseEntity.status(HttpStatus.NO_CONTENT).body(
-//                toJson("Text Entity with ID: $id deleted")
-//            )
-//        }
-//    }
-//}
