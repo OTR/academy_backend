1a536a0129f206e76eae7707d0cead419e78f5aa
OTR <49165533+OTR@users.noreply.github.com>
2023-07-11 12:59:37 +0600

    [FEAT] Implement JOIN TABLE for Track Entity and Project Entity

--- src/main/kotlin/com/github/otr/academy_backend/infrastructure/database/dbo/TrackToProjectEntity.kt
+++ src/main/kotlin/com/github/otr/academy_backend/infrastructure/database/dbo/TrackToProjectEntity.kt
@@ -3,11 +3,11 @@ package com.github.otr.academy_backend.infrastructure.database.dbo
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToMany
 import jakarta.persistence.ManyToOne
-import jakarta.persistence.MapsId
 import jakarta.persistence.Table
 
 /**
@@ -17,7 +17,9 @@ import jakarta.persistence.Table
 @Table(name="tracks_to_projects")
 data class TrackToProjectEntity(
 
-    @Id val id: Int,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "track_id")
