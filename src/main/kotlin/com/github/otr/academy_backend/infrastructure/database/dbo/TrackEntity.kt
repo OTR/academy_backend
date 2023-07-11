1c39294b6ac3c26be8c7e696eb44615456a27818
OTR <49165533+OTR@users.noreply.github.com>
2023-07-11 06:19:05 +0600

    [FIX] Change Database types of some tracks table columns

--- src/main/kotlin/com/github/otr/academy_backend/infrastructure/database/dbo/TrackEntity.kt
+++ src/main/kotlin/com/github/otr/academy_backend/infrastructure/database/dbo/TrackEntity.kt
@@ -1,5 +1,6 @@
 package com.github.otr.academy_backend.infrastructure.database.dbo
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.ManyToMany
@@ -17,9 +18,15 @@ data class TrackEntity(
     @ManyToMany(mappedBy = "tracks")
     val categories: List<CategoryEntity>,
 
+    @Column(columnDefinition = "CLOB")
     val title: String,
+
+    @Column(columnDefinition = "CLOB")
     val description: String,
+
+    @Column(columnDefinition = "CLOB")
     val longDescription: String,
+
     val isBeta: Boolean,
     val isFree: Boolean,
     val isPublic: Boolean,
